51a8e1a9e3c5fa793ceac172313fe3954ebadfaf
Hoàng Hữu Giáp <hoanghuugiap241@gmail.com>
2024-03-27 16:42:23 +0700

    Add If Statement lesson

---
+++ src/IfStatement.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n1 = 10
+    val n2 = 20
+    if (n1 > n2) {
+        println("n1 is greater than n2")
+    } else {
+        println("n1 is not greater than n2")
+    }
+
+
+}
