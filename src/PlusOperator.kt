f4c6b14b152caa6be31c6239904b030bf9c8253a
Hoàng Hữu Giáp <hoanghuugiap241@gmail.com>
2024-03-27 15:37:15 +0700

    Add Plus Operator lesson

---
+++ src/PlusOperator.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val sum = 10 + 10
+    val sumString = "10" + "10"
+    val sumString1 = "1000" + 12312
+//    val minusString = "1000" - 12312 // error
+    println(sum)
+    println(sumString)
+    println(sumString1)
+}
