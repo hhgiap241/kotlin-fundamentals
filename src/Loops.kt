57bd0edc5adb9de26516a4b0dbf231ef75d36ced
Hoàng Hữu Giáp <hoanghuugiap241@gmail.com>
2024-03-28 15:51:27 +0700

    Add Loops lesson

--- src/Loops.kt
+++ src/Loops.kt
@@ -1,8 +1,22 @@
+import kotlin.math.PI
+
 fun main() {
     val names = listOf("mary", "ali", "alex")
     val numbers = intArrayOf(1, 2, 3 ,4, 5)
     for(i in 1..12) {
         println(i)
+    }
+
+    for(i in 5..1 step 2) {
+        println(i)
+    }
+
+    for(i in 5 downTo 1) {
+        println(i)
+    }
+
+    for (name in names) {
+        println(name)
     }
 
     for (index in names.indices) {
